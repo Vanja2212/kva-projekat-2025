4352fa1296782dc67272384644d1d1a7f43cefe0
Vanja2212 <vanja.dragojevic.22@singimail.rs>
2025-02-26 13:30:53 +0100

    Initial upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
