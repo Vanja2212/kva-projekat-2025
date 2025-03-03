60d1b33641803b590d4dd2a38bd9d7b5acfbba68
Vanja2212 <vanja.dragojevic.22@singimail.rs>
2025-03-03 12:32:07 +0100

    initial upload

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -4,22 +4,21 @@ import { FlightModel } from '../../models/flight.model';
 import { NgIf } from '@angular/common';
 import { FlightService } from '../../services/flight.service';
 import { MatButtonModule } from '@angular/material/button';
-import { DatePipe } from '@angular/common';
+import { UtilsService } from '../../services/utils.service';
+import { LoadingComponent } from "../loading/loading.component";
 
 @Component({
-selector: 'app-search',
-imports: [MatTableModule, NgIf, MatButtonModule,DatePipe],
-templateUrl: './search.component.html',
-styleUrl: './search.component.css'
+  selector: 'app-search',
+  imports: [MatTableModule, NgIf, MatButtonModule, LoadingComponent],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
 })
-
 export class SearchComponent {
-displayedColumns: string[] = ['id', 'destination', 'flightNumber', 'scheduledAt', 'actions'];
-dataSource: FlightModel[] | null = null
+  displayedColumns: string[] = ['id', 'destination', 'flightNumber', 'scheduledAt', 'actions'];
+  dataSource: FlightModel[] | null = null
 
-
-public constructor() {
-   FlightService.getFlights(0, 8)
-     .then(rsp => this.dataSource = rsp.data.content)
- }
+  public constructor(public utils: UtilsService) {
+    FlightService.getFlights(0, 8)
+      .then(rsp => this.dataSource = rsp.data.content)
+  }
 }
